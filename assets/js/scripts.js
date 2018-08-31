1070bb495514d33685a87d5a3cd4952ecc1cb1b6
Paulo Militante <p.militante@creativehope.jp>
2018-08-31 18:56:52 +0800

    Edited animation; About page initial

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,39 +4,30 @@ $(document).ready(function(){
 		$(this).toggleClass('animated pulse hovered');
 	});
 
+  $('.my-links a').hover(function(){
+    $(this).toggleClass('animated tada hovered');
+  });
+
 	$('.logo').hover(function(){
 		$(this).toggleClass('animated swing')
 	});
 
-  $('#loader img').toggleClass('animated swing infinite slow');
+// CAROUSEL -----------------------------------------------------------
 
-  setTimeout(function(){
-    $('#loader').toggleClass('animated fadeOutRight')
-    $('.container').toggleClass('animated fadeInLeft')
-    $('.logo').toggleClass('animated swing slow')
+  setTimeout(function(){ 
 
-      // TYPEWRITER
-
-  var app = document.getElementById('typewriter');
-
-  var typewriter = new Typewriter(app, {
-      loop: true
-  });
-
-  typewriter.typeString('I am a Full Stack Web Developer.')
-    .pauseFor(2500)
-    .deleteChars(25)
-    .pauseFor(500)
-    .typeString('problem-solver.')
-    .pauseFor(2500)
-    .deleteChars(15)
-    .pauseFor(500)
-    .typeString('dreamer.')
-    .pauseFor(2500)
-    .deleteAll()
-    .start();
+    $('.tech').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
 
   },3000)
+  
+
+// TYPEWRITER - LOADER -----------------------------------------------------------
 
   var loading = document.getElementById('loading');
 
@@ -48,5 +39,45 @@ $(document).ready(function(){
     .pauseFor(2500)
     .deleteAll()
     .start();    
+
+// LOADER - ANIMATION -----------------------------------------------------------
+
+  $('#loader img').toggleClass('animated swing infinite slower');
+
+  setTimeout(function(){
+    $('.container').css("display","block");
+    $('#loader').toggleClass('animated bounceOutRight slow');
+    $('.container nav').toggleClass('animated bounceInLeft slow');
+    $('.logo').toggleClass('animated swing slower delay-1s');
+    $('.container main').toggleClass('animated bounceInRight slow');
+    $('.container footer').toggleClass('animated bounceInUp slow');
+
+
+    // TYPEWRITER - LANDING
+
+    var app = document.getElementById('typewriter');
+
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+
+    typewriter.typeString('I am a Full Stack Web Developer.')
+      .pauseFor(2500)
+      .deleteChars(25)
+      .pauseFor(500)
+      .typeString('problem-solver.')
+      .pauseFor(2500)
+      .deleteChars(15)
+      .pauseFor(500)
+      .typeString('dreamer.')
+      .pauseFor(2500)
+      .deleteAll()
+      .start();
+
+  },3000);
+
+  setTimeout(function(){
+    $('.logo').toggleClass('animated swing slower delay-1s');
+  },7500);
 
 });
